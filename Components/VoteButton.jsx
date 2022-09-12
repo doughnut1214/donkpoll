71f95f87be5168a3645defdb1216845985b42d70
doughnut1214 <doughnut1214@gmail.com>
2022-09-12 12:24:35 -0400

    CSS changes to question page

--- Components/VoteButton.jsx
+++ Components/VoteButton.jsx
@@ -2,9 +2,9 @@ import styles from '../styles/Home.module.css'
 const VoteButton = (props) => {
 
     return (
-        <div className={styles.card}>
-            <h1>{props.likes}</h1>
-            <button key={props.id} value={props.value} onClick = {props.clickInstruction} disabled={props.disabled}><h1>{props.prompt}</h1></button>
+        <div className="w-4/5 lg:w-1/2  border items-center p-1 pb-0 ml-auto mr-auto mt-2 rounded-lg">
+            <h1 className="text-center">{props.likes}</h1>
+            <button key={props.id} className="w-full bg-slate-500 hover:bg-slate-400 disabled:bg-slate-300 text-white rounded-lg p-3" value={props.value} onClick = {props.clickInstruction} disabled={props.disabled}><h1>{props.prompt}</h1></button>
 
         </div>
 
